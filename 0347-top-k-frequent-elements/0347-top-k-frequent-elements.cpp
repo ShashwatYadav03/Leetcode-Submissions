2dcc0076bb47d9c9395bdbf187e0add33abf32a6
Shashwat Yadav <2k23.cscys2311762@gmail.com>
2026-03-26 15:49:53 +0530

    Time: 5 ms (24.21%), Space: 18 MB (62.09%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+     priority_queue<pair<int,int>> pq;
+       map<int, int> m;
+       for(auto x:nums)
+          m[x]++;
+        for(auto x:m){
+            pq.push({x.second, x.first});
+        }
+        vector<int>ans;
+        while(k--){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;   
+    }
+};
